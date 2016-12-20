5489fe6eec5d954ad28e3b4c19c3013e03b52510
Igor Shevchenko <igoro.shevchenko@gmail.com>
2016-12-20 21:24:06 +0100

    Removed Date

--- src/main/java/com/igor/setup/SingleLineFormatter.java
+++ src/main/java/com/igor/setup/SingleLineFormatter.java
@@ -2,20 +2,24 @@ package com.igor.setup;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
 public class SingleLineFormatter extends Formatter {
 
+	private static final ZoneId ZONE_ID = ZoneId.systemDefault();
+	private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("kk:mm:ss.SSS");
+
 	@Override
 	public String format(LogRecord record) {
 
 		String threadName = Thread.currentThread().getName();
-		Date recordDate = new Date(record.getMillis());
-		DateFormat dateFormat = new SimpleDateFormat("kk:mm:ss.SSS");
+		Instant recordInstant = Instant.ofEpochMilli(record.getMillis());
+		LocalDateTime recordDateTime = LocalDateTime.ofInstant(recordInstant, ZONE_ID);
 
 		StringBuilder sb = new StringBuilder(180);
 		sb.append("[");
@@ -23,7 +27,7 @@ public class SingleLineFormatter extends Formatter {
 		sb.append("] ");
 		sb.append(record.getLevel());
 		sb.append(" ");
-		sb.append(dateFormat.format(recordDate));
+		sb.append(recordDateTime.format(FORMATTER));
 		sb.append(" - ");
 		sb.append(record.getLoggerName());
 		sb.append(": ");
